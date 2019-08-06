9c24621104bd28a4b305356ed782d01b61904dfa
Carrie Cook <carriecook82@gmail.com>
2019-08-05 22:07:55 -0600

    flask

--- Simple_Flask/app.py
+++ Simple_Flask/app.py
@@ -12,11 +12,41 @@ from keras import backend as K
 from flask import Flask, request, redirect, url_for, jsonify
 from keras.models import load_model
 
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+
 app = Flask(__name__)
+
+# Use flask_pymongo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
 app.config['UPLOAD_FOLDER'] = 'Uploads'
+
 
 model = None
 graph = None
+
+@app.route("/")
+def index():
+    mars_dict = mongo.db.mars_collection.find_one()
+    return render_template("index.html", mars=mars_dict)
+
+
+@app.route("/scrape")
+def scraper():
+    mars_collection = mongo.db.mars_collection
+    mars_data = scrape_mars.scrape()
+    mars_collection.update({}, mars_data, upsert=True)
+    return redirect("/", code=302)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+app = Flask(__name__)
+
+
+
 
 
 def prep_model():
